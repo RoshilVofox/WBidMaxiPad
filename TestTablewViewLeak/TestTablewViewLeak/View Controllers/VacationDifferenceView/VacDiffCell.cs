69e54f56e383e46c5d01cd433fc22a650670d10a
Roshil <roshil@vofoxsolutions.com>
2022-04-09 16:09:23 +0530

    Vac diff UI

--- TestTablewViewLeak/TestTablewViewLeak/View Controllers/VacationDifferenceView/VacDiffCell.cs
+++ TestTablewViewLeak/TestTablewViewLeak/View Controllers/VacationDifferenceView/VacDiffCell.cs
@@ -2,6 +2,7 @@
 
 using Foundation;
 using UIKit;
+using WBid.WBidiPad.Model;
 
 namespace TestTablewViewLeak.ViewControllers.VacationDifferenceView
 {
@@ -19,6 +20,25 @@ namespace TestTablewViewLeak.ViewControllers.VacationDifferenceView
         protected VacDiffCell(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
+        }
+        public void LabelValues(FlightDataChangeVacValues flightDataChangeVacValues)
+        {
+            lblLine.Text = flightDataChangeVacValues.LineNum.ToString();
+
+            lblOldTotPay.Text = flightDataChangeVacValues.OldTotalPay.ToString();
+            lblNewTotPay.Text = flightDataChangeVacValues.NewTotalPay.ToString();
+
+            lblOldVpCu.Text = flightDataChangeVacValues.OldVPCu.ToString();
+            lblNewVpCu.Text = flightDataChangeVacValues.NewVPCu.ToString();
+
+            lblOldVpNe.Text = flightDataChangeVacValues.OldVPNe.ToString();
+            lblNewVpNe.Text = flightDataChangeVacValues.NewVPNe.ToString();
+
+            lblOldVpBo.Text = flightDataChangeVacValues.OldVPBo.ToString();
+            lblNewVpBo.Text = flightDataChangeVacValues.NewVPBo.ToString();
+
+
+
         }
     }
 }
--- TestTablewViewLeak/TestTablewViewLeak/View Controllers/VacationDifferenceView/VacDiffTableViewControllerSource.cs
+++ TestTablewViewLeak/TestTablewViewLeak/View Controllers/VacationDifferenceView/VacDiffTableViewControllerSource.cs
@@ -16,11 +16,11 @@ namespace TestTablewViewLeak.ViewControllers.VacationDifferenceView
 
         public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
         {
-            tableView.RegisterNibForCellReuse(UINib.FromName("VacDiffDetailsCell", NSBundle.MainBundle), "VacDiffDetailsCell");
-            // var tes =tableView.DequeueReusableCell(new NSString("VacDiffCell"), indexPath);
+            tableView.RegisterNibForCellReuse(UINib.FromName("VacDiffCell", NSBundle.MainBundle), "VacDiffCell");
+           
 
 
-            VacDiffDetailsCell cell = (VacDiffDetailsCell)tableView.DequeueReusableCell(new NSString("VacDiffDetailsCell"), indexPath);
+            VacDiffCell cell = (VacDiffCell)tableView.DequeueReusableCell(new NSString("VacDiffCell"), indexPath);
             string domicile = string.Empty;
             string position = string.Empty;
             string previousmonthcap = string.Empty;
@@ -29,7 +29,7 @@ namespace TestTablewViewLeak.ViewControllers.VacationDifferenceView
             //previousmonthcap = (lstCAPOutputParameter[indexPath.Row].PreviousMonthCap == null) ? string.Empty : lstCAPOutputParameter[indexPath.Row].PreviousMonthCap.ToString();
             //currentmonthcap = (lstCAPOutputParameter[indexPath.Row].CurrentMonthCap == null) ? string.Empty : lstCAPOutputParameter[indexPath.Row].CurrentMonthCap.ToString();
 
-            //cell.LabelValues(_lstFlightDataChangevalues[indexPath.Row]);
+            cell.LabelValues(_lstFlightDataChangevalues[indexPath.Row]);
             return cell;
         }
 
@@ -49,7 +49,78 @@ namespace TestTablewViewLeak.ViewControllers.VacationDifferenceView
         }
         public override UIView GetViewForHeader(UITableView tableView, nint section)
         {
-            return base.GetViewForHeader(tableView, section);
+          
+
+            UIView view = new UIView(new System.Drawing.RectangleF(0, 0, 443, 20));
+            view.BackgroundColor = UIColor.White;
+
+            UILabel label = new UILabel();
+            label.TextColor = UIColor.Black;
+            label.TextAlignment = UITextAlignment.Center;
+            label.Frame = new System.Drawing.RectangleF(27, 10, 43, 20);
+            label.Text = "Line";
+            view.AddSubview(label);
+
+            UILabel label1 = new UILabel();
+            label1.TextColor = UIColor.Black;
+            label1.TextAlignment = UITextAlignment.Center;
+            label1.Frame = new System.Drawing.RectangleF(142, 10, 43, 20);
+            label1.Text = "Old Tot Pay";
+            view.AddSubview(label1);
+
+            UILabel label2 = new UILabel();
+            label2.TextColor = UIColor.Black;
+            label2.TextAlignment = UITextAlignment.Center;
+            label2.Frame = new System.Drawing.RectangleF(242, 10, 43, 20);
+            label2.Text = "New Tot Pay";
+            view.AddSubview(label2);
+
+            UILabel label3 = new UILabel();
+            label3.TextColor = UIColor.Black;
+            label3.TextAlignment = UITextAlignment.Center;
+            label3.Frame = new System.Drawing.RectangleF(342, 10, 43, 20);
+            label3.Text = "Old VpCu";
+            view.AddSubview(label3);
+
+            UILabel label4 = new UILabel();
+            label4.TextColor = UIColor.Black;
+            label4.TextAlignment = UITextAlignment.Center;
+            label4.Frame = new System.Drawing.RectangleF(442, 10, 43, 20);
+            label4.Text = "New VpCu";
+            view.AddSubview(label4);
+
+
+            UILabel label5 = new UILabel();
+            label5.TextColor = UIColor.Black;
+            label5.TextAlignment = UITextAlignment.Center;
+            label5.Frame = new System.Drawing.RectangleF(542, 10, 43, 20);
+            label5.Text = "Old VpNe";
+            view.AddSubview(label5);
+
+            UILabel label6 = new UILabel();
+            label6.TextColor = UIColor.Black;
+            label6.TextAlignment = UITextAlignment.Center;
+            label6.Frame = new System.Drawing.RectangleF(642, 10, 43, 20);
+            label6.Text = "New VpNe";
+            view.AddSubview(label6);
+
+            UILabel label7 = new UILabel();
+            label7.TextColor = UIColor.Black;
+            label7.TextAlignment = UITextAlignment.Center;
+            label7.Frame = new System.Drawing.RectangleF(742, 10, 43, 20);
+            label7.Text = "Old VpBo";
+            view.AddSubview(label7);
+
+            UILabel label8 = new UILabel();
+            label8.TextColor = UIColor.Black;
+            label8.TextAlignment = UITextAlignment.Center;
+            label8.Frame = new System.Drawing.RectangleF(842, 10, 43, 20);
+            label8.Text = "New VpBo";
+            view.AddSubview(label8);
+
+
+
+            return view;
         }
     }
 }
